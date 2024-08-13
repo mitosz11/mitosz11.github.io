6f93a2a744b36ce1ab4f991a68ef6bb92ba148d1
Bence <bsalgoi@gmail.com>
2024-08-13 18:25:44 +0200

    change picture size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        144: "36rem", 
+        160: "40rem",
+        192: "48rem",
+      },
+    },
   },
   plugins: [],
 };
