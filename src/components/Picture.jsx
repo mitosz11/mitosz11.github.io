f9cb44e5b36523489b180cd851f2d2a878d8b5af
Bence <bsalgoi@gmail.com>
2024-08-14 20:14:34 +0200

    upload new img

---
+++ src/components/Picture.jsx
@@ -0,0 +1,11 @@
+export default function Picture() {
+  return (
+    <div>
+      <img
+        className="w-144 rounded-full drop-shadow-[10px_10px_50px_rgba(14,116,144,0.8)]"
+        src="/me.png"
+        alt="me"
+      />
+    </div>
+  );
+}
